211bfc9e417ae6058f8cd0fba95aef5b625464f0
nuhakhangr8 <nuhakhangr8@gmail.com>
2022-09-03 03:19:34 +0530

    Screen Functionality

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('Helllo World!!!')
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((800,600))
+
+running=True
+
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
